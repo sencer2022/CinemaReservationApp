8a5de7439bec1ef4ccbf527a3f112b7a47d3ceff
sencer2022 <senceralpdogdu@gmail.com>
2022-06-28 19:18:13 +0300

    Calculating price for sleected seats added.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector('.container');
+const count = document.getElementById('count');
+const amount = document.getElementById('amount');
+const select = document.getElementById('movie');
+
 
 container.addEventListener('click', function(e){
     //console.log(e.target);
@@ -6,11 +10,22 @@ container.addEventListener('click', function(e){
     if (e.target.classList.contains('seat') && !e.target.classList.contains('reserved')){
         //console.log('Successful!');
 
-        e.target.classList.toggle('selected');
-        
-    }else{
-        //console.log('Not Successful')
-
+        e.target.classList.toggle('selected'); // toggle: Varsa kaldırır, yoksa ekler.
+        calculateTotal();
     }
+
+
+    select.addEventListener('change', function(e){
+        calculateTotal();
+    });
+
+
+    function calculateTotal(){
+        let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
+        let price = select.value;
+        count.innerText = selectedSeatCount;
+        amount.innerText = selectedSeatCount * price;
+    }
+
 
 });
