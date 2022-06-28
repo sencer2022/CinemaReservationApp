9c6c7f1e9552d99f0b88189639ddbe1a6a007256
sencer2022 <senceralpdogdu@gmail.com>
2022-06-29 00:31:52 +0300

    Saving reservation information to local storage.

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const container = document.querySelector('.container');
 const count = document.getElementById('count');
 const amount = document.getElementById('amount');
 const select = document.getElementById('movie');
+const seats = container.querySelectorAll('.seat:not(.reserved)');
 
 
-container.addEventListener('click', function(e){
+
+container.addEventListener('click', function (e) {
     //console.log(e.target);
 
-    if (e.target.classList.contains('seat') && !e.target.classList.contains('reserved')){
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('reserved')) {
         //console.log('Successful!');
 
         e.target.classList.toggle('selected'); // toggle: Varsa kaldırır, yoksa ekler.
@@ -15,17 +17,46 @@ container.addEventListener('click', function(e){
     }
 
 
-    select.addEventListener('change', function(e){
+    select.addEventListener('change', function (e) {
         calculateTotal();
     });
 
 
-    function calculateTotal(){
-        let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
-        let price = select.value;
+    function calculateTotal() {
+        const selectedSeats = container.querySelectorAll('.seat.selected');
+        //console.log(selectedSeats);
+        //console.log(seats);
+        selectedSeatsArr = [];
+        seatsArr = [];
+
+        selectedSeats.forEach(function(seat){
+            selectedSeatsArr.push(seat);
+        });
+
+        seats.forEach(function(seat){
+            seatsArr.push(seat);
+        });
+
+        let selectedSeatIndexs = selectedSeatsArr.map(function(seat){
+            return seatsArr.indexOf(seat);
+        });
+
+        //console.log(selectedSeatIndexs);
+
+        //console.log(seatsArr);
+        //console.log(selectedSeatsArr);
+
+        let selectedSeatCount = selectedSeats.length;
         count.innerText = selectedSeatCount;
-        amount.innerText = selectedSeatCount * price;
+        amount.innerText = selectedSeatCount * select.value;
+
+        saveToLocalStorage(selectedSeatIndexs);
     }
 
+    function saveToLocalStorage(index){
+        localStorage.setItem('selectedSeats', JSON.stringify(index));
+        console.log(select.selectedIndex);
+        localStorage.setItem('selectedMovieIndex', select.selectedIndex);
+    }
 
 });
